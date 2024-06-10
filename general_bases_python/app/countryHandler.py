a4b23778d8f9bbe6b2a61b042c54c7e82182d606
Ricardo Carvajal <rjsudnew@gmail.com>
2024-06-10 10:56:03 -0400

    type(feat): learnig python __name__ ----------------------------------------- Detailed description:
    BREAKING CHANGE: execution practice
    .py file as script, with word
    reserved __name__
    
    -----------------------------------------
    Create-by: @Ricardo Carvajal
    Reviewed-by: @Ricardo Carvajal
    -----------------------------------------

---
+++ general_bases_python/app/countryHandler.py
@@ -0,0 +1,7 @@
+import main
+
+#Si encapsular el codigo de main en una funcion no podremos
+#obtener solo la lista de los paises, sino que tambien se ejecutara todo
+#el codigo de main.
+#Por lo tanto toca hacer los arreglos necesarios en el main
+print(main.countryList)
--- general_bases_python/app/main.py
+++ general_bases_python/app/main.py
@@ -1,9 +1,5 @@
 import utils 
 
-keys, values = utils.get_population()
-
-print(keys, values)
-
 #Lista de diccionarios con poblacion de cada pais
 countryList = [
     {
@@ -24,8 +20,30 @@ countryList = [
     }
 ]
 
-countryInput = input('Escriba el pais = ')
 
-country = utils.get_population_by_country(countryList, countryInput)
+#Colocamos el codigo principal de main en un metodo para que 
+#si necesitamos importar el main para usarlo en otro archivo
+#como countryHandler, podamos acceder a la lista de paises
+#sin necesidad de ejecutar todo el archivo main.
+def searchCountry():
+    keys, values = utils.get_population()
 
-print(country)
+    print(keys, values)
+
+    countryInput = input('Escriba el pais = ')
+
+    country = utils.get_population_by_country(countryList, countryInput)
+
+    print(country)
+
+#Sin embargo esto anterior nos quita la posibilidad de ejecutar el main.py
+#como un script ya que tendriamos que llamar el metodo desde otro archivo
+#Para poder llamar main.py como un script y a su vez poder importar sus 
+#atributos desde otros archivos podemos hacer lo siguiente:
+
+if __name__ == '__main__':
+    searchCountry()
+
+#Con el uso de la palabra reservada __name__ podemos decirle a python
+#que solo ejecute la linea "searchCountry()" cuando sea llamado desde 
+#la ejecucion del archivo main.py
