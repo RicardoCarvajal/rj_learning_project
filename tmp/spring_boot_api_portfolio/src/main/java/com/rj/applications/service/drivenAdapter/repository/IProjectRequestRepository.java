649d9edbe8da243a72c6d7c3c14a40c35eda8a1c
Ricardo Carvajal <rjsudnew@gmail.com.com>
2024-07-01 19:56:51 -0400

    type(fix): delete temporal archives

--- tmp/spring_boot_api_portfolio/src/main/java/com/rj/applications/service/drivenAdapter/repository/IProjectRequestRepository.java
+++
@@ -1,8 +0,0 @@
-package com.rj.applications.service.drivenAdapter.repository;
-
-import com.rj.applications.service.drivenAdapter.entity.ProjectRequest;
-import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
-
-public interface IProjectRequestRepository extends ReactiveMongoRepository<ProjectRequest, String> {
-
-}
