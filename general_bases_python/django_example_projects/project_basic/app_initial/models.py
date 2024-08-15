7876c9834be4caed63abe0ff47cf8990e4b37e0b
Ricardo Carvajal <rjsudnew@gmail.com>
2024-08-15 12:47:16 -0400

    type(feat): working with django ----------------------------------------- Detailed description:
    BREAKING CHANGE: working relations tables
    with django
    
    -----------------------------------------
    Create-by: @Ricardo Carvajal
    Reviewed-by: @Ricardo Carvajal
    -----------------------------------------

--- general_bases_python/django_example_projects/project_basic/app_initial/models.py
+++ general_bases_python/django_example_projects/project_basic/app_initial/models.py
@@ -8,3 +8,29 @@ class Registry(models.Model):
 
     def __str__(self):
         return f'{self.name} - {self.age} - {self.email} - {self.address}'
+
+class MedicalInfo(models.Model):
+    weight = models.DecimalField(max_digits=5, decimal_places=2)
+    height = models.DecimalField(max_digits=5,decimal_places=2)
+    blod = models.CharField(max_length=3)
+    observation = models.TextField(null=True)
+    registry = models.OneToOneField(Registry, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return f'{self.weight} - {self.height} - {self.blod} - {self.observation}'
+
+class PaymentMethod(models.Model):
+    name = models.CharField(max_length=100)
+    type = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f'{self.name} - {self.type}'
+
+class Pay(models.Model):
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    paydate = models.DateField()
+    registry = models.ForeignKey(Registry,on_delete=models.PROTECT)
+    paymentMethod = models.ManyToManyField(PaymentMethod)
+
+    def __str__(self):
+        return f'{self.amount} - {self.paydate} - {self.registry} - {self.paymentMethod}'
