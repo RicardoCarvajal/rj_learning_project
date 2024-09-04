593e3f32a8fbe15815821725909432c1f4cd257a
Ricardo Carvajal <rjsudnew@gmail.com>
2024-09-04 08:42:33 -0400

    type(feat): change dependency injection

--- general_bases_python/django_example_projects/project_django_db_use/container/container.py
+++ general_bases_python/django_example_projects/project_django_db_use/container/container.py
@@ -1,16 +1,33 @@
-from dependency_injector import containers, providers
 from repository.quotation.quotation_repository_django import QueryObjectQuotationDjango
-from repository.quotation_det.quotation_det_repository_django import QueryObjectQuotationDetDjango
+from repository.quotation_det.quotation_det_repository_django import (
+    QueryObjectQuotationDetDjango,
+)
 from use_case.qutation_get_use_case import QuotationGetUseCase
 
-# Contenedor de dependencias
-class Container(containers.DeclarativeContainer):
-    #Repositorios
-    query_objecto_quotation = providers.Singleton(QueryObjectQuotationDjango)
-    query_objecto_quotation_det = providers.Singleton(QueryObjectQuotationDetDjango)
 
-    #Casos de uso
-    get_quotation_use_case = providers.Factory(QuotationGetUseCase,
-        queryobject=query_objecto_quotation,
-        queryobjectdet=query_objecto_quotation_det
-    )
+# Contenedor de dependencias
+class Container:
+    def __init__(self):
+        self._query_object_quotation = None
+        self._query_object_quotation_det = None
+
+    # Repositorios
+    @property
+    def query_object_quotation(self):
+        if self._query_object_quotation is None:
+            self._query_object_quotation = QueryObjectQuotationDjango()
+        return self._query_object_quotation
+
+    @property
+    def query_object_quotation_det(self):
+        if self._query_object_quotation_det is None:
+            self._query_object_quotation_det = QueryObjectQuotationDetDjango()
+        return self._query_object_quotation_det
+
+    # Casos de uso
+    @property
+    def get_quotation_use_case(self):
+        return QuotationGetUseCase(
+            queryobject=self.query_object_quotation,
+            queryobjectdet=self.query_object_quotation_det,
+        )
--- general_bases_python/django_example_projects/project_django_db_use/main.py
+++ general_bases_python/django_example_projects/project_django_db_use/main.py
@@ -2,7 +2,7 @@ from container.container import Container
 
 container = Container()
 
-service = container.get_quotation_use_case()
+service = container.get_quotation_use_case
 
 queote = service.get_quote_to_send('0020000271')
 
