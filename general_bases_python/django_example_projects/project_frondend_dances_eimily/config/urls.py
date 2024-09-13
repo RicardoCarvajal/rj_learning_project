0166181c0aaeaa29c88364393599cd560fe083e8
Ricardo Carvajal <rjsudnew@gmail.com>
2024-09-13 12:50:08 -0400

    type(feat): working css in django

--- general_bases_python/django_example_projects/project_frondend_dances_eimily/config/urls.py
+++ general_bases_python/django_example_projects/project_frondend_dances_eimily/config/urls.py
@@ -18,9 +18,11 @@ from django.contrib import admin
 from django.conf.urls.static import static
 from django.conf import settings
 from django.urls import path,include
+from dance_events.views import DanceEventsView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', DanceEventsView.as_view()),
     path('events/', include('dance_events.urls')),
     path('users/', include('users.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- general_bases_python/django_example_projects/project_frondend_dances_eimily/dance_events/urls.py
+++ general_bases_python/django_example_projects/project_frondend_dances_eimily/dance_events/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import DanceEventsView, DanceEventsFormView
 
 urlpatterns = [
-    path('list/', DanceEventsView.as_view(),name='list_events'),
+    path('', DanceEventsView.as_view(),name='list_events'),
     path('add/', DanceEventsFormView.as_view(),name='add_events'),
 ] 
