4c3aaae536f1cd4c061ed5c652d82619e4052b3d
Ricardo Carvajal <rjsudnew@gmail.com>
2024-09-18 11:56:33 -0400

    type(feat): creating the test unit for two app django

--- general_bases_python/django_example_projects/project_frondend_dances_eimily/dance_events/tests.py
+++ general_bases_python/django_example_projects/project_frondend_dances_eimily/dance_events/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from django.urls import reverse
+from .models import DanceEnvent
 
-# Create your tests here.
+
+class DanceEventsViewTests(TestCase):
+    def test_return_200(self):
+        url = reverse("list_events")
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_return_events(self):
+        DanceEnvent.objects.create(
+            name="Volviendo a soñar", description="Evento de pruebas"
+        )
+        url = reverse("list_events")
+        response = self.client.get(url)
+        self.assertEqual(response.context["list_events"].count(), 1)
--- general_bases_python/django_example_projects/project_frondend_dances_eimily/dance_tickets/tests.py
+++ general_bases_python/django_example_projects/project_frondend_dances_eimily/dance_tickets/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
+from django.urls import reverse
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+
+class TicketViewTest(TestCase):
+
+    # Prueba si la redireccion cuando no
+    # esta logueado el usuario es correcta
+    def test_logged_user_redirect(self):
+        url = reverse("list_tickets")
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.url, "/users/login/?next=/tickets/list/")
+
+    # Prueba si la redireccion cuando esta
+    # logueado el usuario es correcta
+    def test_no_logged_user_redirect(self):
+        url = reverse("list_tickets")
+        user = get_user_model().objects.create(username="emacarvajal")
+        self.client.force_login(user)
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
