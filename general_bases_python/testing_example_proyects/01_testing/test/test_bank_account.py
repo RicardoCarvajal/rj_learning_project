3e4a949ff2985b6c23d69ce5fcad8a8869dd8bf6
Ricardo Carvajal <rjsudnew@gmail.com>
2024-09-30 16:13:50 -0400

    type(feat): working with testing in python

--- general_bases_python/testing_example_proyects/01_testing/test/test_bank_account.py
+++ general_bases_python/testing_example_proyects/01_testing/test/test_bank_account.py
@@ -1,10 +1,19 @@
+import os
 import unittest
 from src.bank_account import BankAccount
 
 class TestBankAccount(unittest.TestCase):
 
     def setUp(self):
-        self.account = BankAccount(balance=1000)
+        self.account = BankAccount(balance=1000, log_file="transaction.txt")
+
+    def tearDown(self):
+        if os.path.exists(self.account.log_file):
+            os.remove(self.account.log_file)
+
+    def _count_lines(self, filename):
+        with open(filename, "r") as f:
+            return len(f.readlines())
     
     def test_deposit(self):
         new_balance = self.account.deposit(500)
@@ -16,3 +25,12 @@ class TestBankAccount(unittest.TestCase):
 
     def test_get_balance(self):
         assert self.account.get_balance() == 1000
+
+    def test_log_transaction(self):
+        self.account.deposit(500)
+        assert os.path.exists("transaction.txt")
+
+    def test_log_transaction_count_line(self):
+        assert self._count_lines(self.account.log_file) == 1
+        self.account.deposit(500)
+        assert self._count_lines(self.account.log_file) == 2
