f0df4f0a6cb688f7b624c6c5ce3882a54f7c4d03
Ricardo Carvajal <rjsudnew@gmail.com>
2024-10-01 14:56:50 -0400

    type(feat): working with terraform creating variables

--- general_bases_cloud/terraform_basics/05_practica_variable_aws/variables.tf
+++ general_bases_cloud/terraform_basics/05_practica_variable_aws/variables.tf
@@ -1,5 +1,17 @@
 variable "vpc_oregon_cdir" {
+  type        = string
+  sensitive   = false
+  description = "CIDR de oregon"
 }
 
 variable "vpc_virginia_cdir" {
+  type        = string
+  sensitive   = true
+  description = "CIDR de virginia"
+}
+
+variable "vpc_cdir" {
+  type        = list(string)
+  sensitive   = false
+  description = "lista de CIDR"
 }
--- general_bases_cloud/terraform_basics/05_practica_variable_aws/vpc.tf
+++ general_bases_cloud/terraform_basics/05_practica_variable_aws/vpc.tf
@@ -1,14 +1,25 @@
-resource "aws_vpc" "rjcarvajal-oregon-tf-test-vpc" {
+resource "aws_vpc" "rjcarvajal-oregon-tf-test-vpc-1" {
   cidr_block = var.vpc_oregon_cdir
   tags = {
-    env = "devate"
+    env  = "devate"
+    Name = "rjcarvajal-oregon-tf-test-vpc-1"
+  }
+}
+
+resource "aws_vpc" "rjcarvajal-oregon-tf-test-vpc-2" {
+  cidr_block = var.vpc_cdir[2]
+  tags = {
+    env  = "devate"
+    Name = "rjcarvajal-oregon-tf-test-vpc-2"
   }
 }
 
 resource "aws_vpc" "rjcarvajal-virginia-tf-test-vpc" {
   cidr_block = var.vpc_virginia_cdir
   tags = {
-    env = "devate"
+    env  = "devate"
+    Name = "rjcarvajal-virginia-tf-test-vpc"
   }
   provider = aws.virginia
 }
+
