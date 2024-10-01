97ae830731f503df14049f27d3a3ca48f8ea780d
Ricardo Carvajal <rjsudnew@gmail.com>
2024-10-01 16:18:49 -0400

    type(feat): working with terraform creating data

---
+++ general_bases_cloud/terraform_basics/07_practica_ec2_aws/data.tf
@@ -0,0 +1,3 @@
+data "aws_ssm_parameter" "dbdbcredential" {
+  name = "/dev/ate/cr/bopos/parameters/article/dbdbcredential"
+}
--- general_bases_cloud/terraform_basics/07_practica_ec2_aws/outputs.tf
+++ general_bases_cloud/terraform_basics/07_practica_ec2_aws/outputs.tf
@@ -2,3 +2,8 @@ output "ec2_public_ip" {
     value = aws_instance.rjcarvajal-oregon-tf-test-ec2.public_ip
     description = "IP publica de la instancia ec2"
 }
+
+output "parameter" {
+    value = data.aws_ssm_parameter.dbdbcredential.value
+    description = "Parametro que contienen la ARN del secret de la bd"
+}
