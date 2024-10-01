6c31ff6b1763c8d08e725f26321b3b2898ac6dba
Ricardo Carvajal <rjsudnew@gmail.com>
2024-10-01 18:43:14 -0400

    type(feat): working with terraform creating provisioners

--- general_bases_cloud/terraform_basics/07_practica_ec2_aws/instances.tf
+++ general_bases_cloud/terraform_basics/07_practica_ec2_aws/instances.tf
@@ -1,10 +1,33 @@
 resource "aws_instance" "rjcarvajal-oregon-tf-test-ec2" {
-  ami           = "ami-08d8ac128e0a1b91c"
-  instance_type = "t2.micro"
-  subnet_id     = aws_subnet.rjcarvajal-oregon-tf-test-subnet-public.id
-  key_name      = data.aws_key_pair.key-pair.key_name
-  security_groups = [ aws_security_group.rjcarvajal-oregon-tf-test-sg.id ]
+  ami             = "ami-08d8ac128e0a1b91c"
+  instance_type   = "t2.micro"
+  subnet_id       = aws_subnet.rjcarvajal-oregon-tf-test-subnet-public.id
+  key_name        = data.aws_key_pair.key-pair.key_name
+  security_groups = [aws_security_group.rjcarvajal-oregon-tf-test-sg.id]
   tags = {
     Name = "rjcarvajal-oregon-tf-test-ec2"
   }
+  provisioner "local-exec" {
+    command = "echo intancia creada con IP ${aws_instance.rjcarvajal-oregon-tf-test-ec2.public_ip} >> datos.txt"
+  }
+
+  provisioner "local-exec" {
+    when    = destroy
+    command = "echo intancia destruida con IP ${self.public_ip} >> datos.txt"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "echo 'hola mundo' > ~/saludo.txt"
+    ]
+
+    connection {
+      type        = "ssh"
+      host        = self.public_ip
+      user        = "ec2-user"
+      private_key = file("rjcarvajal-oregon-tf-test-key-per.pem")
+    }
+
+  }
+
 }
