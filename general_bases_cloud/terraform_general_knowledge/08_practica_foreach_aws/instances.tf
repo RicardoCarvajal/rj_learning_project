44b825fd0ec45415cc42eed8af646dfa8c91d8da
Ricardo Carvajal <rjsudnew@gmail.com>
2024-10-02 21:30:14 -0400

    type(feat): working with terraform creating instances

--- general_bases_cloud/terraform_general_knowledge/08_practica_foreach_aws/instances.tf
+++ general_bases_cloud/terraform_general_knowledge/08_practica_foreach_aws/instances.tf
@@ -10,7 +10,7 @@ resource "aws_instance" "rjcarvajal-oregon-tf-test-ec2" {
   instance_type   = "t2.micro"
   subnet_id       = aws_subnet.rjcarvajal-oregon-tf-test-subnet-public.id
   key_name        = data.aws_key_pair.key-pair.key_name
-  security_groups = [aws_security_group.rjcarvajal-oregon-tf-test-sg.id]
+  vpc_security_group_ids = [aws_security_group.rjcarvajal-oregon-tf-test-sg.id]
   tags = {
     Name = each.value
   }
