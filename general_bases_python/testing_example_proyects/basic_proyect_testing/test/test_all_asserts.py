1f81a1fe556733a3b57704ebdaa2167238e1a105
Ricardo Carvajal <rjsudnew@gmail.com>
2024-10-24 12:49:36 -0400

    type(feat): working with unittest doing skip in test

--- general_bases_python/testing_example_proyects/basic_proyect_testing/test/test_all_asserts.py
+++ general_bases_python/testing_example_proyects/basic_proyect_testing/test/test_all_asserts.py
@@ -1,9 +1,10 @@
 import unittest
 
+env="prod"
+
 class ClassTest(unittest.TestCase):
     
     def test_assert_equal(self):
-        self.assertEqual(10,"10")
         self.assertEqual("Hola","Hola")
         self.assertNotEqual("Hola","Chao")
 
@@ -27,3 +28,24 @@ class ClassTest(unittest.TestCase):
     def test_assert_set(self):
         numbers = {1,2,2}
         self.assertSetEqual({1,2,2},numbers)
+
+    @unittest.skip("Saltando prueba de función test_assert_skip")
+    def test_assert_skip(self):
+        self.assertEqual(100,100)
+
+    @unittest.skipIf(env == "prod",f"Saltando prueba de función test_assert_skip_if cuando el ambiente sea {env}")
+    def test_assert_skip_if(self):
+        self.assertEqual(100,100)
+
+    @unittest.skipUnless(env == "dev",f"Saltando prueba de función test_assert_skip_if cuando el ambiente sea diferente a dev")
+    def test_assert_skip_unless(self):
+        self.assertEqual(10,"10")   
+
+    @unittest.expectedFailure
+    def test_assert_skip_expected(self):
+        self.assertEqual(10,"10")
+
+    def test_assert_skip_no_decorator(self):
+       if env == "prod":
+           raise unittest.SkipTest("Se salta la prueba test_assert_skip_no_decorator por no cumplir con la condicion")
+       self.assertEqual(10,"10")
