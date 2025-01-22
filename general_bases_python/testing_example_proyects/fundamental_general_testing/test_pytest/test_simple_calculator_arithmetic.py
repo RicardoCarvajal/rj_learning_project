f916fe4726ac3f79b5100e7c73569b0b226c5b3a
Ricardo Carvajal <rjsudnew@gmail.com>
2025-01-22 09:18:14 -0400

    type(feat): add files in proyect test for test in pytest and unitest

--- general_bases_python/testing_example_proyects/fundamental_general_testing/test_pytest/test_simple_calculator_arithmetic.py
+++ general_bases_python/testing_example_proyects/fundamental_general_testing/test_pytest/test_simple_calculator_arithmetic.py
@@ -1,4 +1,18 @@
+import pytest
 from src.simple_calculator_arithmetic import SimpleCalculatorArithmetic as c
 
 def test_sum():
     assert c.sum(2,2) == 4
+
+def test_substrac():
+    assert c.substrac(4,2) == 2
+
+def test_multiply():
+    assert c.multiply(2,2) == 4
+
+def test_divide():
+    assert c.divide(4,2) == 2
+
+def test_divide_zero():
+    with pytest.raises(ValueError,match="No se puede dividir entre 0"):
+        c.divide(4,0)
--- general_bases_python/testing_example_proyects/fundamental_general_testing/test_unnittest/test_simple_calculator_arithmetic.py
+++ general_bases_python/testing_example_proyects/fundamental_general_testing/test_unnittest/test_simple_calculator_arithmetic.py
@@ -6,4 +6,16 @@ class SimpleCalculatorArithmeticTest(unittest.TestCase):
     def test_sum(self):
         self.assertEqual(c.sum(2,2),4)
 
+    def test_substrac(self):
+        self.assertEqual(c.substrac(4,2),2)
+
+    def test_multiply(self):
+        self.assertEqual(c.multiply(2,2),4)
+
+    def test_divide(self):
+        self.assertEqual(c.divide(4,2),2)
+
+    def test_divide_zero(self):
+        with self.assertRaises(ValueError):
+            c.divide(4,0)
     
